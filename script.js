f1ff879b2853c4c9a2c32658c48f9ba2279d3a12
Dasha Grebenchuk <d.a.malevanchenko@gmail.com>
2022-06-13 02:14:11 +0200

    fixed the API URL error with API key

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ function formatDate(timestamp) {
   return formedDate;
 }
 //forecast
-function dispalyForecast() {
+function dispalyForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
@@ -47,8 +48,8 @@ function dispalyForecast() {
 }
 function getForecast(coordinates) {
   let apiKey = "4fb36667f50c716efb0c9e559b5b7ffe";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(dispalyForecast);
 }
 //change the city when clicking Search + currentWeather
 function showCurrentWeather(response) {
